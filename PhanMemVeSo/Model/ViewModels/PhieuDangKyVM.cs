3895f12fe84df04383fa208b22512c4d49035095
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2018-10-13 13:32:27 +0700

    Upload

---
+++ PhanMemVeSo/Model/ViewModels/PhieuDangKyVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ViewModels
+{
+    public class PhieuDangKyVM
+    {
+        public int LoaiVeSoId { get; set; }
+        public string TenTinh { get; set; }
+        public decimal SLDangKy { get; set; }
+
+    }
+}
